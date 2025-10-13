e3539fd5e618962f23b67aa268cbf81127059b46
originalhawk242 <oghawk242@gmail.com>
2025-10-13 12:43:20 -0700

    added intake telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Intake.java
@@ -4,27 +4,28 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Intake{
     public static final String INTAKE_MOTOR_NAME = "Intake";
-
-    //change speed accordingly in auto or teleop classes
-    public static double engagePower = 0.5;
-
     private final DcMotor intakeMotor;
+    private final Telemetry telemetry;
 
-    public Intake(HardwareMap hwMap){
-        intakeMotor= hwMap.get(DcMotor.class, INTAKE_MOTOR_NAME);
+    public Intake(HardwareMap hardwareMap, Telemetry telemetry){
+        intakeMotor = hardwareMap.get(DcMotor.class, INTAKE_MOTOR_NAME);
+
+        this.telemetry = telemetry;
     }
     public void setPower(double power){
         intakeMotor.setPower(power);
-    }
 
-    public void engageIntake(){
-        intakeMotor.setPower(engagePower);
+        telemetry.addData("Intake: ", power);
     }
 
     public void stopIntake(){
         intakeMotor.setPower(0);
+
+        telemetry.addData("Intake: ", "stopped");
     }
 
 }
