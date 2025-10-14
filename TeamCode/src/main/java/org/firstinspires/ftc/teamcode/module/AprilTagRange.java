b59f1a46749b35abdd6350fa24be5389d2779eb4
originalhawk242 <oghawk242@gmail.com>
2025-10-14 12:46:28 -0700

    renamed AprilTagOdometry.java to AprilTagRange.java, updated references changed getTagPosition() to getTagRange(), now returns range to apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/AprilTagRange.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/AprilTagRange.java
@@ -1,17 +1,14 @@
 package org.firstinspires.ftc.teamcode.module;
 
-import androidx.annotation.Nullable;
-
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
-public class AprilTagOdometry {
+public class AprilTagRange {
     public static final String WEBCAM_NAME = "Webcam 1";
 
     private final AprilTagProcessor processor;
@@ -19,7 +16,7 @@ public class AprilTagOdometry {
 
     private final Telemetry telemetry;
 
-    public AprilTagOdometry(HardwareMap hardwareMap, Telemetry telemetry) {
+    public AprilTagRange(HardwareMap hardwareMap, Telemetry telemetry) {
         processor = AprilTagProcessor.easyCreateWithDefaults();
         visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, WEBCAM_NAME))
@@ -28,17 +25,16 @@ public class AprilTagOdometry {
         this.telemetry = telemetry;
     }
 
-    @Nullable
-    public Position getTagPosition(int tagID) {
+    public double getTagRange(int tagID) {
         telemetry.addData("Tag ID: ", tagID);
         for (AprilTagDetection detection : processor.getDetections()) {
             if (detection.id == tagID) {
                 telemetry.addData("Tag Status: " , tagID + "found");
-                return detection.robotPose.getPosition();
+                return detection.ftcPose.range;
             } else {
                 telemetry.addData("Tag Status: ", tagID + "not found");
             }
         }
-        return null;
+        return -1.0;
     }
 }
