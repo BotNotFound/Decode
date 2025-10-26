ab511647e913ce0fdfaf2f8eedc1cd6bb78c8183
FreehandBlock51 <freehandblock51@outlook.com>
2025-10-26 14:56:50 -0700

    Added SquIDController.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquIDController.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+public class SquIDController {
+    private double kP;
+    private double tolerance;
+    private double target;
+    private double curError;
+
+    public SquIDController(double kP) {
+        this.kP = kP;
+    }
+
+    public void setP(double kP) {
+        this.kP = kP;
+    }
+
+    public void setTolerance(double tolerance) {
+        this.tolerance = tolerance;
+    }
+
+    public void setTarget(double targetValue) {
+        this.target = targetValue;
+    }
+
+    public boolean atTarget() {
+        return Math.abs(curError) <= tolerance;
+    }
+
+    public double calculate(double measuredValue) {
+        curError = measuredValue - target;
+        if (atTarget()) {
+            return 0.0;
+        }
+
+        return kP * Math.sqrt(curError);
+    }
+}
