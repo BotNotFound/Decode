111856a290325860438ea23df94c834a7f0426e3
CodeBrainNeeded <95329066+CodeBrainNeeded@users.noreply.github.com>
2025-11-08 10:22:55 -0800

    Far Auto Red and Blue
    Created two autos for the far launch zone and leave for both red and blue

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/FarAutoBlue.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.BezierPoint;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.opmode.teleop.TwoPersonTeleOpRed;
+
+@Autonomous(name = "far auto blue", group = "blue", preselectTeleOp = TwoPersonTeleOpRed.OP_MODE_NAME)
+public class FarAutoBlue extends AutonomousBase{
+
+    public FarAutoBlue() {
+        super(new Pose(84.432, 8.407, Math.toRadians(180-90)).mirror(), Robot.AllianceColor.BLUE);
+    }
+    @Override
+    protected AutonomousStage[] buildStageSequence() {
+        Paths autoPath = new Paths(follower);
+        return new AutonomousStage[]{
+                new AutonomousStage(autoPath.shootPreloads, Robot.RobotState.PRE_SHOOT),
+                new AutonomousStage(
+                        follower.pathBuilder().addPath(new BezierPoint(autoPath.shootPreloads.endPose()))
+                                .setConstantHeadingInterpolation(Math.toRadians(180-72))
+                                .build(),
+                        Robot.RobotState.SHOOT
+                ),
+                new AutonomousStage(autoPath.leavingLaunchZone, Robot.RobotState.NONE)
+        };
+    }
+
+    public static class Paths {
+
+        public PathChain shootPreloads;
+        public PathChain leavingLaunchZone;
+
+        public Paths(Follower follower) {
+            shootPreloads = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierCurve(
+                                    new Pose(84.432, 8.407).mirror(),
+                                    new Pose(86.758, 11.806).mirror(),
+                                    new Pose(83.001, 11.091).mirror()
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(180-90), Math.toRadians(180-72))
+                    .build();
+
+            leavingLaunchZone = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(83.001, 11.091).mirror(), new Pose(110.191, 13.058).mirror())
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(180-72), Math.toRadians(180))
+                    .build();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/FarAutoRed.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.BezierPoint;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.opmode.teleop.TwoPersonTeleOpRed;
+
+@Autonomous(name = "far auto red", group = "red", preselectTeleOp = TwoPersonTeleOpRed.OP_MODE_NAME)
+public class FarAutoRed extends AutonomousBase{
+
+    public FarAutoRed() {
+        super(new Pose(84.432, 8.407, Math.toRadians(90)), Robot.AllianceColor.RED);
+    }
+    @Override
+    protected AutonomousStage[] buildStageSequence() {
+        Paths autoPath = new Paths(follower);
+        return new AutonomousStage[]{
+                new AutonomousStage(autoPath.shootPreloads, Robot.RobotState.PRE_SHOOT),
+                new AutonomousStage(
+                        follower.pathBuilder().addPath(new BezierPoint(autoPath.shootPreloads.endPose()))
+                                .setConstantHeadingInterpolation(Math.toRadians(72))
+                                .build(),
+                        Robot.RobotState.SHOOT
+                ),
+                new AutonomousStage(autoPath.leavingLaunchZone, Robot.RobotState.NONE)
+        };
+    }
+
+    public static class Paths {
+
+        public PathChain shootPreloads;
+        public PathChain leavingLaunchZone;
+
+        public Paths(Follower follower) {
+            shootPreloads = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierCurve(
+                                    new Pose(84.432, 8.407),
+                                    new Pose(86.758, 11.806),
+                                    new Pose(83.001, 11.091)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(72))
+                    .build();
+
+            leavingLaunchZone = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(83.001, 11.091), new Pose(110.191, 13.058))
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(72), Math.toRadians(0))
+                    .build();
+        }
+    }
+}
