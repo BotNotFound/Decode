769beb5fde75c1b6c67b3935e717f346f0edd074
FreehandBlock51 <freehandblock51@outlook.com>
2025-11-15 14:35:05 -0800

    Moved the recorded auto classes into their own package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/recording/OpModeRecord.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/recording/OpModeRecord.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmode;
+package org.firstinspires.ftc.teamcode.opmode.recording;
 
 import android.os.Environment;
 import android.util.Log;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/recording/Recorder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/recording/Recorder.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmode;
+package org.firstinspires.ftc.teamcode.opmode.recording;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
