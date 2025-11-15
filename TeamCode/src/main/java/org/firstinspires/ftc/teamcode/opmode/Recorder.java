3f8898dfcb88c76cdb50a074497899af3a7c50c2
FreehandBlock51 <freehandblock51@outlook.com>
2025-11-14 23:08:42 -0800

    [recordable autos] recordings now can be named

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Recorder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Recorder.java
@@ -4,11 +4,74 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.opmode.teleop.TeleOp;
 
-import java.util.UUID;
-
 @Autonomous(name = "Record New Autonomous")
 public class Recorder extends TeleOp {
     private OpModeRecord.Recorder recorder;
+
+    private static final String RECORD_NAME_AVAILABLE_CHARS =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ";
+    private StringBuilder recordName;
+    private int recordNameCurIdx;
+
+    private static int rotateInt(int cur, int offset, int max) {
+        int next = cur + offset;
+        while (cur >= max) {
+            cur -= max;
+        }
+        while (cur < 0) {
+            cur += max;
+        }
+        return next;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        recordName = new StringBuilder();
+    }
+
+    @Override
+    public void init_loop() {
+        super.init_loop();
+
+        while (recordNameCurIdx > recordName.length()) {
+            recordNameCurIdx -= recordName.length();
+        }
+        while (recordNameCurIdx < 0) {
+            recordNameCurIdx += recordName.length();
+        }
+        if (recordNameCurIdx == recordName.length()) {
+            recordName.append(RECORD_NAME_AVAILABLE_CHARS.charAt(0));
+        }
+
+        final int curCharIdx = RECORD_NAME_AVAILABLE_CHARS.indexOf(recordName.charAt(recordNameCurIdx));
+        if (gamepad1.dpadUpWasPressed()) {
+            recordName.setCharAt(
+                    recordNameCurIdx,
+                    RECORD_NAME_AVAILABLE_CHARS.charAt(
+                            rotateInt(curCharIdx, 1, RECORD_NAME_AVAILABLE_CHARS.length())
+                    )
+            );
+        }
+        if (gamepad1.dpadDownWasPressed()) {
+            recordName.setCharAt(
+                recordNameCurIdx,
+                RECORD_NAME_AVAILABLE_CHARS.charAt(
+                        rotateInt(curCharIdx, -1, RECORD_NAME_AVAILABLE_CHARS.length())
+                )
+            );
+        }
+
+        if (gamepad1.dpadRightWasPressed()) {
+            recordNameCurIdx = rotateInt(recordNameCurIdx, 1, recordName.length());
+        }
+        if (gamepad1.dpadLeftWasPressed()) {
+            recordNameCurIdx = rotateInt(recordNameCurIdx, -1, recordName.length());
+        }
+
+        telemetry.addLine("Record Name [use DPAD to change]:");
+        telemetry.addLine(recordName.toString());
+    }
 
     @Override
     public void start() {
@@ -25,6 +88,6 @@ public class Recorder extends TeleOp {
     @Override
     public void stop() {
         super.stop();
-        Replayer.Hub.registerRecord(UUID.randomUUID().toString(), recorder.finishRecording());
+        Replayer.Hub.registerRecord(recordName.toString(), recorder.finishRecording());
     }
 }
