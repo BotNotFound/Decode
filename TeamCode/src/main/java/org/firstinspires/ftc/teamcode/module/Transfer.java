4dccc7471936f0e0d0492f552ddb3aad76846c2d
FreehandBlock51 <freehandblock51@outlook.com>
2025-11-21 22:01:20 -0800

    Refactored Transfer.java for readability of Transfer.POWER_VALUE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Transfer.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.module;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -17,7 +18,7 @@ public class Transfer {
     public static final String TRANSFER2_SERVO_NAME = "Transfer 2";
 
     //adjust the servo power as needed for the transfer.
-    public static double POWER_VALUE = -1.0;
+    public static double POWER_VALUE = 1.0;
 
     private final Telemetry telemetry;
 
@@ -26,12 +27,15 @@ public class Transfer {
         transferServo = hwMap.get(CRServo.class, TRANSFER_SERVO_NAME);
         transferServo2 = hwMap.get(CRServo.class, TRANSFER2_SERVO_NAME);
 
+        transferServo.setDirection(DcMotorSimple.Direction.REVERSE);
+        transferServo2.setDirection(DcMotorSimple.Direction.FORWARD);
+
         this.telemetry = telemetry;
     }
 
     public void startTransfer() {
         transferServo.setPower(POWER_VALUE);
-        transferServo2.setPower(-POWER_VALUE);
+        transferServo2.setPower(POWER_VALUE);
     }
 
 
@@ -43,7 +47,7 @@ public class Transfer {
     //this method is useful if color sensor detects wrong artifact
     public void reverseTransfer() {
         transferServo.setPower(-POWER_VALUE);
-        transferServo2.setPower(POWER_VALUE);
+        transferServo2.setPower(-POWER_VALUE);
     }
 
 
