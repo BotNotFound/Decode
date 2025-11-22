1907e54a6e739b9f0085c1bce2322957f5269a5a
FreehandBlock51 <freehandblock51@outlook.com>
2025-11-21 16:43:18 -0800

    mirrored SimpleTwelveArtifactRed.java into SimpleTwelveArtifactBlue.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/SimpleTwelveArtifactBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/SimpleTwelveArtifactBlue.java
@@ -12,10 +12,10 @@ import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.opmode.teleop.TwoPersonTeleOpBlue;
 
 @SuppressWarnings("PointlessArithmeticExpression") // arithmetic is applied uniformly to ensure everything is mirrored
-@Autonomous(name = "8 Artifact Blue", group = "blue", preselectTeleOp = TwoPersonTeleOpBlue.OP_MODE_NAME)
-public class EightArtifactBlue extends AutonomousBase {
-    public EightArtifactBlue() {
-        super(new Pose(111.622, 135.414, 0).mirror(), Robot.AllianceColor.BLUE);
+@Autonomous(name = "12 Artifact Blue (simple)", group = "blue", preselectTeleOp = TwoPersonTeleOpBlue.OP_MODE_NAME)
+public class SimpleTwelveArtifactBlue extends AutonomousBase {
+    public SimpleTwelveArtifactBlue() {
+        super(new Pose(106.8, 135.414, Math.toRadians(180 - 0)), Robot.AllianceColor.BLUE);
     }
 
     @Override
@@ -68,7 +68,7 @@ public class EightArtifactBlue extends AutonomousBase {
         };
     }
 
-    @SuppressWarnings("PointlessArithmeticExpression")
+    @SuppressWarnings("PointlessArithmeticExpression") // see rationale at top of file
     public static class Paths {
 
         public final PathChain shootPreloads;
@@ -86,7 +86,7 @@ public class EightArtifactBlue extends AutonomousBase {
             shootPreloads = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(111.622, 135.414).mirror(), new Pose(95.344, 99.816).mirror())
+                            new BezierLine(new Pose(106.800, 135.414).mirror(), new Pose(95.344, 99.816).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-0), Math.toRadians(180-45))
                     .build();
@@ -94,7 +94,7 @@ public class EightArtifactBlue extends AutonomousBase {
             goToFirstRowArtifacts = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(95.344, 99.816).mirror(), new Pose(90, 83.538).mirror())
+                            new BezierLine(new Pose(95.344, 99.816).mirror(), new Pose(90, 84).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-45), Math.toRadians(180-0))
                     .build();
@@ -102,7 +102,7 @@ public class EightArtifactBlue extends AutonomousBase {
             intakeFirstRow = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(90, 83.538).mirror(), new Pose(135, 83.359).mirror())
+                            new BezierLine(new Pose(90, 84).mirror(), new Pose(140, 84).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-0), Math.toRadians(180-0))
                     .build();
@@ -110,7 +110,7 @@ public class EightArtifactBlue extends AutonomousBase {
             shootFirstRow = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(135, 83.359).mirror(), new Pose(99.101, 100.353).mirror())
+                            new BezierLine(new Pose(140, 84).mirror(), new Pose(99.101, 100.353).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-0), Math.toRadians(180-45))
                     .build();
@@ -118,7 +118,7 @@ public class EightArtifactBlue extends AutonomousBase {
             goToSecondRow = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(99.101, 100.353).mirror(), new Pose(90, 60).mirror())
+                            new BezierLine(new Pose(99.101, 100.353).mirror(), new Pose(90, 62).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-45), Math.toRadians(180-0))
                     .build();
@@ -126,7 +126,7 @@ public class EightArtifactBlue extends AutonomousBase {
             intakeSecondRowArtifacts = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(90, 60).mirror(), new Pose(135, 60).mirror())
+                            new BezierLine(new Pose(90, 62).mirror(), new Pose(150, 62).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-0), Math.toRadians(180-0))
                     .build();
@@ -135,7 +135,7 @@ public class EightArtifactBlue extends AutonomousBase {
                     .pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(135, 60).mirror(),
+                                    new Pose(150, 62).mirror(),
                                     new Pose(92.303, 68.154).mirror(),
                                     new Pose(91.051, 92.482).mirror()
                             )
@@ -146,7 +146,7 @@ public class EightArtifactBlue extends AutonomousBase {
             goToThirdRow = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(91.051, 92.482).mirror(), new Pose(90, 36).mirror())
+                            new BezierLine(new Pose(91.051, 92.482).mirror(), new Pose(90, 37).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-45), Math.toRadians(180-0))
                     .build();
@@ -154,7 +154,7 @@ public class EightArtifactBlue extends AutonomousBase {
             intakeThirdRow = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(90, 36).mirror(), new Pose(135, 36).mirror())
+                            new BezierLine(new Pose(90, 37).mirror(), new Pose(150, 37).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-0), Math.toRadians(180-0))
                     .build();
@@ -162,11 +162,10 @@ public class EightArtifactBlue extends AutonomousBase {
             shootThirdRow = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(135, 36).mirror(), new Pose(91.051, 92.482).mirror())
+                            new BezierLine(new Pose(150, 37).mirror(), new Pose(91.051, 92.482).mirror())
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(180-0), Math.toRadians(180-45))
                     .build();
         }
     }
 }
-
