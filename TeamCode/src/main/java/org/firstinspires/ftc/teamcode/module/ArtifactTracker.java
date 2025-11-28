8311c7ddb93b1f599fec6aaac8183ec39f363ede
Aryan Bose <1057221@lwsd.org>
2025-11-27 16:09:02 -0800

    revised the artifact tracker class, more setting led light methods, cleaner logic, and methods for teleop and Robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/ArtifactTracker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/ArtifactTracker.java
@@ -6,7 +6,6 @@ import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import org.firstinspires.ftc.teamcode.module.Intake;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -25,17 +24,15 @@ public class ArtifactTracker {
     private final RevColorSensorV3 colorSensorFar;
 
     private final Servo LED; 
-    
-
-    private final Telemetry telemetry;
-    
-    
 
 
+    public static final double ARTIFACT_DISTANCE_THRESHOLD_CM = 5;
+
+    private final Telemetry tele;
 
 
-    public ArtifactTracker(HardwareMap hwMap, Telemtry tele){
-        telemetry = tele;
+    public ArtifactTracker(HardwareMap hwMap, Telemetry telemetry){
+        tele = telemetry;
 
         colorSensorNear= hwMap.get(RevColorSensorV3.class, COLOR_SENSOR_NEAR);
         colorSensorMiddle= hwMap.get(RevColorSensorV3.class, COLOR_SENSOR_MIDDLE);
@@ -45,79 +42,82 @@ public class ArtifactTracker {
 
     }
     private boolean hasBall(RevColorSensorV3 sensor) {
-        return sensor.getDistance(DistanceUnit.CM) <= 5;
+        double dist = sensor.getDistance(DistanceUnit.CM);
+        return !Double.isNaN(dist) && dist <= ARTIFACT_DISTANCE_THRESHOLD_CM;
+
     }
     public boolean hasAllArtifacts(){
-        //return hasBall(colorSensorNear) && hasBall(colorSensorMiddle) && hasBall(colorSensorFar);
-        if(numArtifacts()!=3){
-            return false;
-        }
-        tele.addData("Robot ", "has all artifacts");
-        LED.setPosition(0.500);
-        return true;
+        return (numArtifacts() == 3) || (hasBall(colorSensorNear) && 
+        hasBall(colorSensorMiddle) && hasBall(colorSensorFar));
     }
     public boolean hasNoArtifacts(){
-        //return !hasBall(colorSensorNear) && !hasBall(colorSensorMiddle) && !hasBall(colorSensorFar);
-        if(numArtifacts()!= 0){
-            return false;
-        }
-        tele.addData("Robot ", "has no artifacts");
-        LED.setPosition(0.277);
-        return true;
+        return (numArtifacts() == 0) || (!hasBall(colorSensorNear) && 
+        !hasBall(colorSensorMiddle) && !hasBall(colorSensorFar));
+        
     }
 
     public boolean hasSomeArtifacts(){
-        if(numArtifacts()>0 && numArtifacts()<3){
-
-            if(colorSensorNear.hasBall()){
-                tele.addData("Artifact ", "detected at the front of the robot");
-            }
-            else{
-                tele.addData("Artifact ", "NOT detected at the front of the robot");
-            }
-            if(colorSensorMiddle.hasBall()){
-                tele.addData("Artifact ", "detected in the middle of the robot");
-            }
-            else{
-                tele.addData("Artifact ", "NOT detected in the middle of the robot");
-            }
-            if(colorSensorFar.hasBall()){
-                tele.addData("Artifact ", "detected at the back of the robot");
-            }
-            else{
-                tele.addData("Artifact ", "NOT detected at the back of the robot");
-            }
-            LED.setPosition(0.388);
-            return true;
-        }
-        return false;
+        return numArtifacts()>0 && numArtifacts()<3; 
+        
     }
-    
+    /*This is because when we have four artifacts the fourth ball get pushed up the hood and the color sensor at
+     * the back will think its too far. This is the only cases I know for when there are 4 artifacts in our robot.
+     */
     public boolean tooManyArtifacts(){
+        return hasBall(colorSensorNear) && hasBall(colorSensorMiddle) && !hasBall(colorSensorFar);
+    }
 
-        if((!colorSensorNear.hasBall() && !colorSensorFar.hasBall())
-        || (colorSensorNear.hasBall() && !colorSensorFar.hasBall())){
-            LED.setPosition(0.722);
-            return true;
-        }
-        return false; 
-
+    public void setLEDViolet(){
+        tele.addData("CAREFUL!! ", "YOU HAVE TOO MANY ARTIFACTS!! REVERSE INTAKE NOW!!");
+        tele.update();
+        LED.setPosition(0.722);
     }
     
-    public int numArtifacts(){
+    public void setLEDYellow(){
+        tele.addData("ROBOT HAS SOME BUT NOT ALL ARTIFACTS!! ", "KEEP INTAKING!!");
+        tele.update();
+        LED.setPosition(0.388);
+    }
+    public void setLEDRed(){
+        tele.addData("THE ROBOT IS EMPTY!! ", "START INTAKING!!");
+        tele.update();
+        LED.setPosition(0.277);
+    }
+    public void setLEDGreen(){
+        tele.addData("THE ROBOT IS FULL OF ARTIFACTS!!", "GO SHOOT NOW!!!");
+        tele.update();
+        LED.setPosition(0.500);
+    }
+    
+    
+    public int numArtifacts() {
         int count = 0;
-        if(colorSensorNear.hasBall()){
-            count++;
-        }
-        if(colorSensorMiddle.hasBall()){
-            count++;
-        }
-        if(colorSensorFar.hasBall()){
-            count++;
-        }
+        if (hasBall(colorSensorNear)) count++;
+        if (hasBall(colorSensorMiddle)) count++;
+        if (hasBall(colorSensorFar)) count++;
         return count;
     }
+
+    public void reportDetections(){
+        if(hasBall(colorSensorNear)){
+            tele.addData("Artifact ", "detected at the front of the robot");
+        }
+        else{
+            tele.addData("Artifact ", "NOT detected at the front of the robot");
+        }
+        if(hasBall(colorSensorMiddle)){
+            tele.addData("Artifact ", "detected in the middle of the robot");
+        }
+        else{
+            tele.addData("Artifact ", "NOT detected in the middle of the robot");
+        }
+        if(hasBall(colorSensorFar)){
+            tele.addData("Artifact ", "detected at the back of the robot");
+        }
+        else{
+            tele.addData("Artifact ", "NOT detected at the back of the robot");
+        }
+        tele.update(); 
+    }
     
-    //if the second the third one detect a artifact then stop the transfer 
-    //if the second and third one have an artifact 
 }
