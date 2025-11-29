c2b726eec31691db7004df4d556b30311019a01d
FreehandBlock51 <freehandblock51@outlook.com>
2025-11-28 17:02:16 -0800

    reformatting and renaming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/ArtifactTracker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/ArtifactTracker.java
@@ -28,37 +28,40 @@ public class ArtifactTracker {
 
     public static final double ARTIFACT_DISTANCE_THRESHOLD_CM = 5;
 
-    private final Telemetry tele;
+    private final Telemetry telemetry;
 
 
-    public ArtifactTracker(HardwareMap hwMap, Telemetry telemetry){
-        tele = telemetry;
+    public ArtifactTracker(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
 
-        colorSensorNear= hwMap.get(RevColorSensorV3.class, COLOR_SENSOR_NEAR);
-        colorSensorMiddle= hwMap.get(RevColorSensorV3.class, COLOR_SENSOR_MIDDLE);
-        colorSensorFar= hwMap.get(RevColorSensorV3.class, COLOR_SENSOR_FAR);
-        LED = hwMap.get(Servo.class, "LED Light");
+        colorSensorNear = hardwareMap.get(RevColorSensorV3.class, COLOR_SENSOR_NEAR);
+        colorSensorMiddle = hardwareMap.get(RevColorSensorV3.class, COLOR_SENSOR_MIDDLE);
+        colorSensorFar = hardwareMap.get(RevColorSensorV3.class, COLOR_SENSOR_FAR);
+        LED = hardwareMap.get(Servo.class, "LED Light");
 
 
     }
+
     private boolean hasBall(RevColorSensorV3 sensor) {
         double dist = sensor.getDistance(DistanceUnit.CM);
         return !Double.isNaN(dist) && dist <= ARTIFACT_DISTANCE_THRESHOLD_CM;
 
     }
-    public boolean hasAllArtifacts(){
-        return (numArtifacts() == 3) || (hasBall(colorSensorNear) && 
-        hasBall(colorSensorMiddle) && hasBall(colorSensorFar));
-    }
-    public boolean hasNoArtifacts(){
-        return (numArtifacts() == 0) || (!hasBall(colorSensorNear) && 
-        !hasBall(colorSensorMiddle) && !hasBall(colorSensorFar));
-        
+
+    public boolean hasAllArtifacts() {
+        return (numArtifacts() == 3) || (hasBall(colorSensorNear) &&
+                hasBall(colorSensorMiddle) && hasBall(colorSensorFar));
     }
 
-    public boolean hasSomeArtifacts(){
-        return numArtifacts()>0 && numArtifacts()<3; 
-        
+    public boolean hasNoArtifacts() {
+        return (numArtifacts() == 0) || (!hasBall(colorSensorNear) &&
+                !hasBall(colorSensorMiddle) && !hasBall(colorSensorFar));
+
+    }
+
+    public boolean hasSomeArtifacts() {
+        return numArtifacts() > 0 && numArtifacts() < 3;
+
     }
     /*This is because when we have four artifacts the fourth ball get pushed up the hood and the color sensor at
      * the back will think its too far. This is the only cases I know for when there are 4 artifacts in our robot.
@@ -68,57 +71,65 @@ public class ArtifactTracker {
         return hasBall(colorSensorNear) && hasBall(colorSensorMiddle) && !hasBall(colorSensorFar);
     }*/
 
-    public void setLEDViolet(){
-        tele.addData("CAREFUL!! ", "YOU HAVE TOO MANY ARTIFACTS!! REVERSE INTAKE NOW!!");
-        tele.update();
+    public void setLEDViolet() {
+        telemetry.addData("CAREFUL!! ", "YOU HAVE TOO MANY ARTIFACTS!! REVERSE INTAKE NOW!!");
+        telemetry.update();
         LED.setPosition(0.722);
     }
-    
-    public void setLEDYellow(){
-        tele.addData("ROBOT HAS SOME BUT NOT ALL ARTIFACTS!! ", "KEEP INTAKING!!");
-        tele.update();
+
+    public void setLEDYellow() {
+        telemetry.addData("ROBOT HAS SOME BUT NOT ALL ARTIFACTS!! ", "KEEP INTAKING!!");
+        telemetry.update();
         LED.setPosition(0.388);
     }
-    public void setLEDRed(){
-        tele.addData("THE ROBOT IS EMPTY!! ", "START INTAKING!!");
-        tele.update();
+
+    public void setLEDRed() {
+        telemetry.addData("THE ROBOT IS EMPTY!! ", "START INTAKING!!");
+        telemetry.update();
         LED.setPosition(0.277);
     }
-    public void setLEDGreen(){
-        tele.addData("THE ROBOT IS FULL OF ARTIFACTS!!", "GO SHOOT NOW!!");
-        tele.update();
+
+    public void setLEDGreen() {
+        telemetry.addData("THE ROBOT IS FULL OF ARTIFACTS!!", "GO SHOOT NOW!!");
+        telemetry.update();
         LED.setPosition(0.500);
     }
-    
-    
+
+
     public int numArtifacts() {
         int count = 0;
-        if (hasBall(colorSensorNear)) count++;
-        if (hasBall(colorSensorMiddle)) count++;
-        if (hasBall(colorSensorFar)) count++;
+        if (hasBall(colorSensorNear)) {
+            count++;
+        }
+        if (hasBall(colorSensorMiddle)) {
+            count++;
+        }
+        if (hasBall(colorSensorFar)) {
+            count++;
+        }
         return count;
     }
 
-    public void reportDetections(){
-        if(hasBall(colorSensorNear)){
-            tele.addData("Artifact ", "detected at the front of the robot");
+    public void reportDetections() {
+        if (hasBall(colorSensorNear)) {
+            telemetry.addData("Artifact ", "detected at the front of the robot");
         }
-        else{
-            tele.addData("Artifact ", "NOT detected at the front of the robot");
+        else {
+            telemetry.addData("Artifact ", "NOT detected at the front of the robot");
         }
-        if(hasBall(colorSensorMiddle)){
-            tele.addData("Artifact ", "detected in the middle of the robot");
+        if (hasBall(colorSensorMiddle)) {
+            telemetry.addData("Artifact ", "detected in the middle of the robot");
         }
-        else{
-            tele.addData("Artifact ", "NOT detected in the middle of the robot");
+        else {
+            telemetry.addData("Artifact ", "NOT detected in the middle of the robot");
         }
-        if(hasBall(colorSensorFar)){
-            tele.addData("Artifact ", "detected at the back of the robot");
+        if (hasBall(colorSensorFar)) {
+            telemetry.addData("Artifact ", "detected at the back of the robot");
         }
-        else{
-            tele.addData("Artifact ", "NOT detected at the back of the robot");
+        else {
+            telemetry.addData("Artifact ", "NOT detected at the back of the robot");
         }
-        tele.update(); 
+        telemetry.update();
     }
-    
+
 }
