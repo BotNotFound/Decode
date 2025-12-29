4846da5442949b7b31595e88fd884b8903a7920e
originalhawk242 <oghawk242@gmail.com>
2025-12-29 15:25:02 -0800

    reset turret on opmode start v1 (no way this actually works pls help me)
    also added motor encoder to turret angle conversion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/TurretAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/TurretAim.java
@@ -10,11 +10,13 @@ public class TurretAim {
     public static final String TURRET_MOTOR_NAME = "Turret";
     private final DcMotor turretMotor;
     private final Telemetry telemetry;
+
     // TODO: figure out what these need to be
     public static final double RED_GOAL_ANGLE = 0;
     public static final double BLUE_GOAL_ANGLE = 0;
     private double goalAngle = RED_GOAL_ANGLE;
-    public static final double POINTS_PER_REVOLUTION = 537.7;
+    private static final double POINTS_PER_REVOLUTION = 537.7;
+    private static final double TURRET_GEAR_CONVERSION = 1;
 
     public TurretAim(HardwareMap hardwareMap, Telemetry telemetry, AllianceColor allianceColor) {
         turretMotor = hardwareMap.get(DcMotor.class, TURRET_MOTOR_NAME);
@@ -25,8 +27,13 @@ public class TurretAim {
 
         turretMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         turretMotor.setTargetPosition(0);
-        turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         turretMotor.setPower(1);
+        turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // TODO: motor.isBusy() loop here, im not sure whether to have that in Robot or here
         turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    private static double motorToTurretAngle(double motorAngle) {
+        return motorAngle * TURRET_GEAR_CONVERSION;
     }
 }
