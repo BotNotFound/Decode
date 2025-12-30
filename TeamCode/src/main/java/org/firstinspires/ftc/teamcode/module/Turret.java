a4771282ed67b5f350503cc59ae251d58b4defcb
originalhawk242 <oghawk242@gmail.com>
2025-12-29 16:25:16 -0800

    removed telemetry from Turret.java changed setSetPoint for aimAtGoal()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Turret.java
@@ -20,29 +20,23 @@ public class Turret {
     public static double kF = 0;
     private final PIDFController aimController;
 
-    private final double goalAngle;
-
-    private final Telemetry telemetry;
-
     /**
      * The number of encoder ticks in a single revolution of the motor We are currently using a
      * <a href="https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-5-2-1-ratio-24mm-length-8mm-rex-shaft-1150-rpm-3-3-5v-encoder/">
-     *     5203 goBuilda motor
+     *     5203 goBilda motor
      * </a>
      * and a 113:12 gear ratio
      */
     public static final double TICKS_PER_REVOLUTION = 145.1 * (113.0 / 12.0);
 
-    public Turret(HardwareMap hardwareMap, Telemetry telemetry, AllianceColor allianceColor) {
+    public Turret(HardwareMap hardwareMap, AllianceColor allianceColor) {
         turretMotor = hardwareMap.get(DcMotor.class, TURRET_MOTOR_NAME);
-        this.telemetry = telemetry;
-        goalAngle = allianceColor.goalAngle;
         aimController = new PIDFController(kP, kI, kD, kF);
     }
 
-    public void aimAtGoal(double curHeading) {
+    public void aimAtGoal(double x, double y, double curHeading) {
         aimController.setPIDF(kP, kI, kD, kF);
-        aimController.setSetPoint(goalAngle);
+        aimController.setSetPoint(Math.atan2(y, x));
         turretMotor.setPower(aimController.calculate(curHeading));
     }
 
