e88ed8b5d0acc69a5ac06dc99ff4378dfebd69ac
FreehandBlock51 <freehandblock51@outlook.com>
2025-12-30 19:05:02 -0800

    completed a task

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Spindexer.java
@@ -40,7 +40,6 @@ public class Spindexer {
     //TODO: Tune the artifact distance threshold for the color sensor so it detects reliably
     public static double ARTIFACT_DISTANCE_THRESHOLD_CM = 2;
 
-    //TODO: Tune the below angles and power values
     public static double offsetAngle = 0;
 
     // TODO tune PIDF values + tolerance
