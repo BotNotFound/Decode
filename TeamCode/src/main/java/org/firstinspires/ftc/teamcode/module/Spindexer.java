46613805de9aecb7e94d90305b7cf12d51451a5e
FreehandBlock51 <freehandblock51@outlook.com>
2025-12-31 16:39:52 -0800

    fixed using degrees in a radian field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Spindexer.java
@@ -36,7 +36,7 @@ public class Spindexer {
 
     private static final String FRONT_COLOR_SENSOR = "Front Color Sensor";
 
-    public static double LOADING_ANGLE_OFFSET = 180.0;
+    public static double LOADING_ANGLE_OFFSET = Math.PI;
 
     private final CRServo spindexerServoOne;
     private final CRServo spindexerServoTwo;
