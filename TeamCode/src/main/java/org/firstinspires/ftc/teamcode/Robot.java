ba343afb317891715376000bca11d45b0aeeefbe
FreehandBlock51 <freehandblock51@outlook.com>
2026-01-01 17:58:19 -0800

    fixed bad robot dimensions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -22,8 +22,8 @@ import org.firstinspires.ftc.teamcode.module.Turret;
 public class Robot {
     private static final String TAG = "Robot";
 
-    private static final double ROBOT_LENGTH_IN = 17.5; // TODO get correct robot length
-    private static final double ROBOT_WIDTH_IN = 17.3;
+    private static final double ROBOT_LENGTH_IN = 17.3;
+    private static final double ROBOT_WIDTH_IN = 17.5;
     private static final Pose2D DEFAULT_ROBOT_POSE = new Pose2D(
             DistanceUnit.INCH,
             144.0 - (ROBOT_LENGTH_IN / 2),
