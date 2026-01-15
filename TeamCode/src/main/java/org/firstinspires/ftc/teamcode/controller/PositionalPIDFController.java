c8da3e2feb5dc2fc704be3a39e660f56eaf02414
FreehandBlock51 <freehandblock51@outlook.com>
2026-01-14 18:02:52 -0800

    created PositionalPIDFController.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controller/PositionalPIDFController.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.controller;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+
+public class PositionalPIDFController extends PIDController {
+    private double feedforwardThreshold = 1;
+
+    public PositionalPIDFController(double kp, double ki, double kd, double kf) {
+        super(kp, ki, kd);
+        setF(kf);
+    }
+
+    @Override
+    public double calculate(double pv) {
+        final double pidPower = super.calculate();
+        final double error = getPositionError();
+        if (Math.abs(error) < feedforwardThreshold) {
+            return pidPower;
+        }
+        return pidPower + getF() * Math.signum(error);
+    }
+
+    public double getFeedforwardThreshold() {
+        return feedforwardThreshold;
+    }
+
+    public void setFeedforwardThreshold(double kfErrorThreshold) {
+        this.feedforwardThreshold = kfErrorThreshold;
+    }
+}
