137db4fe6575aa6840b959c3c2e1ab558670b4f4
FreehandBlock51 <freehandblock51@outlook.com>
2026-01-14 17:42:02 -0800

    fixed pinpoint offsets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/FieldCentricDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/FieldCentricDriveTrain.java
@@ -62,7 +62,7 @@ public class FieldCentricDriveTrain {
         this.telemetry = telemetry;
 
         pinpointDriver = hardwareMap.get(GoBildaPinpointDriver.class, PINPOINT_DRIVER_NAME);
-        pinpointDriver.setOffsets(174, 20, DistanceUnit.MM);
+        pinpointDriver.setOffsets(-301.5, 16, DistanceUnit.MM);
         pinpointDriver.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
         pinpointDriver.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
         pinpointDriver.resetPosAndIMU();
