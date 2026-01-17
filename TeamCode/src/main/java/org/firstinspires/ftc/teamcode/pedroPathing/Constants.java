2a2f9e02968f2bf21ade9abb57ae754e4f6445ad
FreehandBlock51 <freehandblock51@outlook.com>
2026-01-17 00:39:05 -0800

    tuned heading pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
         .forwardZeroPowerAcceleration(-37.132714179943896)
         .lateralZeroPowerAcceleration(-76.01161208193545)
         .translationalPIDFCoefficients(new PIDFCoefficients(0.2, 0, 0.02, 0.03))
-        .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.1, 0.03))
+        .headingPIDFCoefficients(new PIDFCoefficients(0.5, 0, 0, 0.05))
         .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.01, 0, 0.001, 0, 0.03))
         .centripetalScaling(0.05);
 
